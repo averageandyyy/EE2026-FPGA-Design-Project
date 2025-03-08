9badb6afd8433ca79168756df045de82059d5193
WayneCh0y <e1155251@u.nus.edu>
2025-03-08 22:20:19 +0800

    Implementation of Part B

--- FDP.srcs/sources_1/new/Basic_Task_B.v
+++ FDP.srcs/sources_1/new/Basic_Task_B.v
@@ -19,44 +19,22 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+// Student ID: A 0 2 8 1 1 2 3 R
+// Password: 0, 1, 2, 3, 8 
 
-module basic_task_b(input basys_clock, btnU, btnC, btnD, output [7:0] JB);
+module basic_task_b(input basys_clock, btnU, btnC, btnD, [12:0] pixel_index, input hasPassword, output reg [15:0] oled_data = 0);
     // When the program starts, three solid white squares of 13x13 pixels should appear vertically on the OLED. All the other parts are black.
     // OLED display: 96x64 (x by y)
     
-    reg [31:0] counter_6p25 = 7;
     reg [31:0] counter_25 = 1;
     reg [31:0] counter_1 = 49999;
-    wire my_6p25mHz_signal;
     wire my_25mHz_signal;
     wire my_1kHz_signal;
     
-    flexible_clock_divider my_6p25mHz(.main_clock(basys_clock), .ticks(counter_6p25), .output_clock(my_6p25mHz_signal));
     flexible_clock_divider my_25mHz(.main_clock(basys_clock), .ticks(counter_25), .output_clock(my_25mHz_signal));
     flexible_clock_divider my_1kHz(.main_clock(basys_clock), .ticks(counter_1), .output_clock(my_1kHz_signal));
 
     reg reset = 0;
-    reg [15:0] oled_data = 0;
-    wire frame_begin;
-    wire sending_pixels;
-    wire sample_pixel;
-    wire [12:0] pixel_index;
-    
-    Oled_Display display(
-    .clk(my_6p25mHz_signal), 
-    .reset(reset), 
-    .frame_begin(frame_begin), 
-    .sending_pixels(sending_pixels),
-    .sample_pixel(sample_pixel), 
-    .pixel_index(pixel_index), 
-    .pixel_data(oled_data), 
-    .cs(JB[0]), 
-    .sdin(JB[1]), 
-    .sclk(JB[3]), 
-    .d_cn(JB[4]), 
-    .resn(JB[5]), 
-    .vccen(JB[6]),
-    .pmoden(JB[7]));
     
     wire [6:0] x;
     wire [6:0] y;
@@ -76,29 +54,35 @@ module basic_task_b(input basys_clock, btnU, btnC, btnD, output [7:0] JB);
     reg btnU_prev = 0, btnC_prev = 0, btnD_prev = 0;
     
     always @ (posedge my_1kHz_signal) begin
+        if (hasPassword) begin
+            if (debounce_counter_U > 0) debounce_counter_U <= debounce_counter_U - 1;
+            if (debounce_counter_C > 0) debounce_counter_C <= debounce_counter_C - 1;
+            if (debounce_counter_D > 0) debounce_counter_D <= debounce_counter_D - 1;
     
-        if (debounce_counter_U > 0) debounce_counter_U <= debounce_counter_U - 1;
-        if (debounce_counter_C > 0) debounce_counter_C <= debounce_counter_C - 1;
-        if (debounce_counter_D > 0) debounce_counter_D <= debounce_counter_D - 1;
-    
-        if (btnU && !btnU_prev && debounce_counter_U == 0) begin
-            color_top_idx <= (color_top_idx >= 3'b101) ? 0 : color_top_idx + 1;
-            debounce_counter_U <= 200; 
-        end
+            if (btnU && !btnU_prev && debounce_counter_U == 0) begin
+                color_top_idx <= (color_top_idx >= 3'b101) ? 0 : color_top_idx + 1;
+                debounce_counter_U <= 200; 
+            end
 
-        if (btnC && !btnC_prev && debounce_counter_C == 0) begin
-            color_middle_idx <= (color_middle_idx >= 3'b101) ? 0 : color_middle_idx + 1;
-            debounce_counter_C <= 200;
-        end
+            if (btnC && !btnC_prev && debounce_counter_C == 0) begin
+                color_middle_idx <= (color_middle_idx >= 3'b101) ? 0 : color_middle_idx + 1;
+                debounce_counter_C <= 200;
+            end
 
-        if (btnD && !btnD_prev && debounce_counter_D == 0) begin
-            color_bottom_idx <= (color_bottom_idx >= 3'b101) ? 0 : color_bottom_idx + 1;
-            debounce_counter_D <= 200; 
-        end
+            if (btnD && !btnD_prev && debounce_counter_D == 0) begin
+                color_bottom_idx <= (color_bottom_idx >= 3'b101) ? 0 : color_bottom_idx + 1;
+                debounce_counter_D <= 200; 
+            end
         
-        btnU_prev <= btnU;
-        btnC_prev <= btnC;
-        btnD_prev <= btnD;
+            btnU_prev <= btnU;
+            btnC_prev <= btnC;
+            btnD_prev <= btnD;
+         end
+         else begin
+            color_top_idx <= 3'b000;
+            color_middle_idx <= 3'b000;
+            color_bottom_idx <= 3'b000;
+         end
     end
     
     // Color mapping
@@ -121,32 +105,37 @@ module basic_task_b(input basys_clock, btnU, btnC, btnD, output [7:0] JB);
     // Initialisation
     always @ (posedge my_25mHz_signal)
     begin
-        oled_data <= 16'b00000_000000_00000;
+        if (hasPassword) begin
+            oled_data <= 16'b00000_000000_00000;
 
-        if (x > 41 && x < 54) begin
-            if (y > 2 && y < 15) begin
-                oled_data <= get_color(color_top_idx);
+            if (x > 41 && x < 54) begin
+                if (y > 2 && y < 15) begin
+                    oled_data <= get_color(color_top_idx);
+                end
+                if (y > 17 && y < 30) begin
+                    oled_data <= get_color(color_middle_idx);
+                end
+                if (y > 32 && y < 45) begin
+                    oled_data <= get_color(color_bottom_idx);
+                end       
             end
-            if (y > 17 && y < 30) begin
-                oled_data <= get_color(color_middle_idx);
-            end
-            if (y > 32 && y < 45) begin
-                oled_data <= get_color(color_bottom_idx);
-            end       
+        
+            if (color_top_idx == 3'b001 && color_middle_idx == 3'b001 && color_bottom_idx == 3'b001) begin
+                if ((x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) <= r_squared) begin
+                    oled_data <= 16'b11111_000000_00000; 
+                end        
+            end 
+        
+            if (color_top_idx == 3'b100 && color_middle_idx == 3'b100 && color_bottom_idx == 3'b100) begin
+                if ((x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) <= r_squared) begin
+                    oled_data <= 16'b11111_100101_00000; 
+                end        
+            end 
         end
         
-        if (color_top_idx == 3'b001 && color_middle_idx == 3'b001 && color_bottom_idx == 3'b001) begin
-            if ((x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) <= r_squared) begin
-                oled_data <= 16'b11111_000000_00000; 
-            end        
+        else begin
+            oled_data <= 16'b00000_000000_00000;
         end 
-        
-        if (color_top_idx == 3'b100 && color_middle_idx == 3'b100 && color_bottom_idx == 3'b100) begin
-            if ((x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) <= r_squared) begin
-                oled_data <= 16'b11111_100101_00000; 
-            end        
-        end 
-        
     end
     
     // Up pushbutton = Change topmost square

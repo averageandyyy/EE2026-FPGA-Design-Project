c6853ce22bf7c237a5c76642cf22352d01ae79b8
averageandyyy <averageandyyy@gmail.com>
2025-03-13 19:09:36 +0800

    Feat: Initialize two oled display units

--- FDP.srcs/sources_1/new/Top_Student.v
+++ FDP.srcs/sources_1/new/Top_Student.v
@@ -11,7 +11,69 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Top_Student (input basys_clock, input [7:0] sw, output [7:0]JB);
+module Top_Student (
+    input basys_clock, 
+    input [15:0] sw, 
+    output [15:0] led,
+    input btnC, btnU, btnD, btnL, btnR,
+    output [7:0]JB, // First OLED
+    output [7:0]JA  // Second OLED
+    );
+
+    // 6.25MHz clock for OLED displays
+    wire clk_6p25MHz;
+    flexible_clock_divider clk_6p25MHz_gen(
+        .main_clock(basys_clock),
+        .ticks(7),
+        .output_clock(clk_6p25MHz)
+    );
+
+    // First OLED display unit
+    wire one_frame_begin;
+    wire one_sample_pixel;
+    wire [12:0]one_pixel_index;
+    wire one_sending_pixels;
+    wire [15:0]one_oled_data;
+
+    Oled_Display first_display(
+        .clk(clk_6p25MHz),
+        .reset(0),
+        .frame_begin(one_frame_begin),
+        .sending_pixels(one_sending_pixels),
+        .sample_pixel(one_sample_pixel),
+        .pixel_index(one_pixel_index),
+        .pixel_data(one_oled_data),
+        .cs(JB[0]), 
+        .sdin(JB[1]), 
+        .sclk(JB[3]), 
+        .d_cn(JB[4]), 
+        .resn(JB[5]), 
+        .vccen(JB[6]),
+        .pmoden(JB[7])
+    );
     
+    // Second OLED display unit
+    wire two_frame_begin;
+    wire two_sample_pixel;
+    wire [12:0]two_pixel_index;
+    wire two_sending_pixels;
+    wire [15:0]two_oled_data;
+
+    Oled_Display second_display(
+        .clk(clk_6p25MHz),
+        .reset(0),
+        .frame_begin(two_frame_begin),
+        .sending_pixels(two_sending_pixels),
+        .sample_pixel(two_sample_pixel),
+        .pixel_index(two_pixel_index),
+        .pixel_data(two_oled_data),
+        .cs(JA[0]), 
+        .sdin(JA[1]), 
+        .sclk(JA[3]), 
+        .d_cn(JA[4]), 
+        .resn(JA[5]), 
+        .vccen(JA[6]),
+        .pmoden(JA[7])
+    );
 
 endmodule

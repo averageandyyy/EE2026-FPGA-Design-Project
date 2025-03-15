6634f91f94e4231b777cab55ef54da980f8369b4
averageandyyy <averageandyyy@gmail.com>
2025-03-15 14:01:11 +0800

    Fix: Fixed input_bcd_to_fp_builder - Removed all logic relating to input_buffer. Was not necessary at all   as it was just a string representation for our numbers that was not in   use - Fixed the logic for converting numbers from BCD representation to   fixed point representation. Turns out Verilog/Vivado is not happy when   bounds in loops are variable, so I had to fix some upper bounds.

--- FDP.srcs/sources_1/new/input_bcd_to_fp_builder.v
+++ FDP.srcs/sources_1/new/input_bcd_to_fp_builder.v
@@ -27,12 +27,11 @@ module input_bcd_to_fp_builder(
     input is_operand_mode,
     input reset,
     output reg has_decimal = 0,
-    output reg [63:0]input_buffer,
     output reg [3:0]input_index = 0,
     output reg signed [31:0] fp_value = 0,
     output reg [31:0] bcd_value = 0,
     output reg input_complete = 0,
-    output reg [3:0]decimal_pos = 0
+    output reg [3:0]decimal_pos = 4'hF
     );
 
     // Variables to store 8 BCD digits
@@ -45,11 +44,6 @@ module input_bcd_to_fp_builder(
     parameter BACKSPACE = 4'd11;
     
     integer i;
-    
-    // Loop variables
-    reg [7:0] removed_char;
-    reg [7:0] ch;
-    reg [3:0] digit_pos = 0;
 
     // Loop to update packed BCD value from individual digits
     always @ (*) begin
@@ -58,24 +52,6 @@ module input_bcd_to_fp_builder(
             bcd_digits[3], bcd_digits[2], bcd_digits[1], bcd_digits[0]
         };
     end
-
-    // Helper function to get/set characters in input_buffer
-    function [7:0] get_char;
-        input [3:0] index;
-        begin
-            case(index)
-                0: get_char = input_buffer[7:0];
-                1: get_char = input_buffer[15:8];
-                2: get_char = input_buffer[23:16];
-                3: get_char = input_buffer[31:24];
-                4: get_char = input_buffer[39:32];
-                5: get_char = input_buffer[47:40];
-                6: get_char = input_buffer[55:48];
-                7: get_char = input_buffer[63:56];
-                default: get_char = " ";
-            endcase
-        end
-    endfunction
 
     // Loop to process user input
     always @ (posedge clk) begin
@@ -88,7 +64,6 @@ module input_bcd_to_fp_builder(
             has_decimal <= 0;
             decimal_pos <= 4'hF;
             valid_digits <= 0;
-            input_buffer <= 0;
 
             // We do not reset fp value here since the arithmetic_backend module will use it for computation
 
@@ -105,19 +80,6 @@ module input_bcd_to_fp_builder(
                     if (!has_decimal && input_index < 8) begin
                         has_decimal <= 1;
                         decimal_pos <= valid_digits;
-
-                        // Set decimal point in appropriate position of input_buffer
-                        case(input_index)
-                            0: input_buffer[7:0] <= ".";
-                            1: input_buffer[15:8] <= ".";
-                            2: input_buffer[23:16] <= ".";
-                            3: input_buffer[31:24] <= ".";
-                            4: input_buffer[39:32] <= ".";
-                            5: input_buffer[47:40] <= ".";
-                            6: input_buffer[55:48] <= ".";
-                            7: input_buffer[63:56] <= ".";
-                        endcase
-
                         input_index <= input_index + 1;
                     end
                 end
@@ -125,91 +87,27 @@ module input_bcd_to_fp_builder(
                 BACKSPACE: begin
                     if (input_index > 0) begin
                         input_index <= input_index - 1;
-                        
-                        case(input_index-1)
-                            0: removed_char = input_buffer[7:0];
-                            1: removed_char = input_buffer[15:8];
-                            2: removed_char = input_buffer[23:16];
-                            3: removed_char = input_buffer[31:24];
-                            4: removed_char = input_buffer[39:32];
-                            5: removed_char = input_buffer[47:40];
-                            6: removed_char = input_buffer[55:48];
-                            7: removed_char = input_buffer[63:56];
-                            default: removed_char = " ";
-                        endcase
-
-                        // Check if removing decimal point
-                        if (removed_char == ".") begin
+        
+                        // Check if we're removing decimal point
+                        if (has_decimal && decimal_pos == valid_digits) begin
+                            // We entered decimal as the last character
                             has_decimal <= 0;
                             decimal_pos <= 4'hF;
+                            // No change to digits
                         end
-                        // Removing a digit
-                        else begin
+                        else if (valid_digits > 0) begin
+                            // We're removing a digit
                             valid_digits <= valid_digits - 1;
-                            
-                            // Find position in bcd_digits that corresponds to this character
-                            // (This is complex since we need to map input_index to valid_digits position)
-                            // For simplicity, we'll rebuild the bcd_digits from input_buffer
-                            
-                            
-                            // Clear the bcd array first
-                            for (i = 0; i < 8; i = i + 1) begin
-                                bcd_digits[i] <= 0;
-                            end
-                            
-                            // Rebuild digits from remaining characters
-                            for (i = 0; i < input_index-1; i = i + 1) begin
-                                
-                                
-                                case(i)
-                                    0: ch = input_buffer[7:0];
-                                    1: ch = input_buffer[15:8];
-                                    2: ch = input_buffer[23:16];
-                                    3: ch = input_buffer[31:24];
-                                    4: ch = input_buffer[39:32];
-                                    5: ch = input_buffer[47:40];
-                                    6: ch = input_buffer[55:48];
-                                    7: ch = input_buffer[63:56];
-                                endcase
-                                
-                                if (ch != ".") begin
-                                    bcd_digits[digit_pos] <= ch - "0";
-                                    digit_pos <= digit_pos + 1;
-                                end
-                            end
+            
+                            // Clear the highest (last entered) digit
+                            bcd_digits[valid_digits - 1] <= 0;
                         end
-
-                        // Clear the deleted character in buffer
-                        case(input_index-1)
-                            0: input_buffer[7:0] <= " ";
-                            1: input_buffer[15:8] <= " ";
-                            2: input_buffer[23:16] <= " ";
-                            3: input_buffer[31:24] <= " ";
-                            4: input_buffer[39:32] <= " ";
-                            5: input_buffer[47:40] <= " ";
-                            6: input_buffer[55:48] <= " ";
-                            7: input_buffer[63:56] <= " ";
-                        endcase
-                        
-                        // Shift remaining characters left
-                        for (i = input_index-1; i < 7; i = i + 1) begin
-                            case(i)
-                                0: input_buffer[7:0] <= input_buffer[15:8];
-                                1: input_buffer[15:8] <= input_buffer[23:16];
-                                2: input_buffer[23:16] <= input_buffer[31:24];
-                                3: input_buffer[31:24] <= input_buffer[39:32];
-                                4: input_buffer[39:32] <= input_buffer[47:40];
-                                5: input_buffer[47:40] <= input_buffer[55:48];
-                                6: input_buffer[55:48] <= input_buffer[63:56];
-                            endcase
-                        end
-                        input_buffer[63:56] <= " ";
                     end
                 end
 
                 CHECKMARK: begin
-                    if (input_index > 0) begin
-                        // Converting BCD to fixed point representation
+                    if (valid_digits > 0 || (valid_digits == 0 && has_decimal)) begin
+                        // Convert BCD to fixed-point
                         fp_value <= convert_to_fixed_point(valid_digits, decimal_pos);
                         input_complete <= 1;
                     end
@@ -217,24 +115,12 @@ module input_bcd_to_fp_builder(
 
                 // Process digits
                 default: begin
-                    // Only accept if we have room (max 8 including decimal)
+                    // Only accept if we have room (max 8 digits)
                     if (input_index < 8 && valid_digits < 8) begin
-                        // Store ASCII character for display
-                        case(input_index)
-                            0: input_buffer[7:0] <= selected_keypad_value + "0";
-                            1: input_buffer[15:8] <= selected_keypad_value + "0";
-                            2: input_buffer[23:16] <= selected_keypad_value + "0";
-                            3: input_buffer[31:24] <= selected_keypad_value + "0";
-                            4: input_buffer[39:32] <= selected_keypad_value + "0";
-                            5: input_buffer[47:40] <= selected_keypad_value + "0";
-                            6: input_buffer[55:48] <= selected_keypad_value + "0";
-                            7: input_buffer[63:56] <= selected_keypad_value + "0";
-                        endcase
-
                         // Store BCD digit
                         bcd_digits[valid_digits] <= selected_keypad_value;
 
-                        // Increment indexes
+                        // Update counters
                         input_index <= input_index + 1;
                         valid_digits <= valid_digits + 1;
                     end
@@ -250,7 +136,6 @@ module input_bcd_to_fp_builder(
             valid_digits <= 0;
             fp_value <= 0;
             input_complete <= 0;
-            input_buffer <= 0;  // Clear all characters
 
             for (i = 0; i < 8; i = i + 1) begin
                 bcd_digits[i] <= 0;
@@ -266,7 +151,7 @@ module input_bcd_to_fp_builder(
         reg [31:0] integer_part;
         reg [31:0] fractional_part;
         reg [31:0] power_of_ten;
-        integer i;  // Declare loop variable outside
+        integer i;
     
         begin
             integer_part = 0;
@@ -276,20 +161,27 @@ module input_bcd_to_fp_builder(
             // Calculate integer part
             if (decimal_pos == 4'hF) begin
                 // No decimal point, all digits are integer part
-                for (i = 0; i < valid_digits; i = i + 1) begin
-                    integer_part = (integer_part * 10) + bcd_digits[i];
+                for (i = 0; i < 8; i = i + 1) begin  // Fixed upper bound
+                    if (i < valid_digits) begin       // Only process valid digits
+                        integer_part = (integer_part * 10) + bcd_digits[i];
+                    end
                 end 
             end
             else begin
-                // Process digits before decimal point
-                for (i = 0; i < decimal_pos; i = i + 1) begin
-                    integer_part = (integer_part * 10) + bcd_digits[i];
+                // Process digits before decimal point (fixed bound)
+                for (i = 0; i < 8; i = i + 1) begin   // Fixed upper bound
+                    if (i < decimal_pos) begin        // Only process relevant digits
+                        integer_part = (integer_part * 10) + bcd_digits[i];
+                    end
                 end
 
-                // Digits after decimal point
-                for (i = decimal_pos; i < valid_digits; i = i + 1) begin
-                    power_of_ten = power_of_ten * 10;
-                    fractional_part = fractional_part + (bcd_digits[i] * 65536) / power_of_ten;
+                // Process digits after decimal point (fixed bound)
+                power_of_ten = 1;
+                for (i = 0; i < 8; i = i + 1) begin   // Fixed upper bound
+                    if (i >= decimal_pos && i < valid_digits) begin  // Only process relevant digits
+                        power_of_ten = power_of_ten * 10;
+                        fractional_part = fractional_part + (bcd_digits[i] * 65536) / power_of_ten;
+                    end
                 end
             end
 

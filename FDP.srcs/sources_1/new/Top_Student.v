0cbc0d18ef45e9a984b8c0ecc4989e95b4481f4f
averageandyyy <averageandyyy@gmail.com>
2025-03-27 10:18:27 +0800

    Feat: Tested refactored arithmetic module! To refactor display stuff

--- FDP.srcs/sources_1/new/Top_Student.v
+++ FDP.srcs/sources_1/new/Top_Student.v
@@ -38,58 +38,53 @@ module Top_Student (
         .output_clock(clk_1kHz)
     );
     
-   //mouse part
-    // Default input values for the mouse_module
-       wire [11:0] value;
-       assign value = 12'b0; // Default value is 0 (origin)
-   
-       wire setx;
-       assign setx = 1'b0;   // No update command, keep current position
-   
-       wire sety;
-       assign sety = 1'b0;   // No update command, keep current position
-   
-       wire setmax_x;
-       assign setmax_x= 1'b0; // Do not update max_x
-   
-       wire setmax_y;
-       assign setmax_y = 1'b0; // Do not update max_y
-       wire [11:0] xpos;
-       wire [11:0] ypos;
-       wire [3:0]  zpos;
-       wire        left;
-       wire        middle;
-       wire        right;
-       wire        new_event;
-       wire        rst;        // Reset signal
-    mouse_module unit_0 (
-          .clk       (basys_clock),
-          .rst       (rst),
-          .value     (value),
-          .setmax_x  (setmax_x),
-          .setmax_y  (setmax_y),
-          .setx      (setx),
-          .sety      (sety),
-          .ps2_clk   (ps2_clk),
-          .ps2_data  (ps2_data),
-          .xpos      (xpos),
-          .ypos      (ypos),
-          .zpos      (zpos),
-          .left      (left),
-          .middle    (middle),
-          .right     (right),
-          .new_event (new_event)
-      );
-      //end of mouse part
-
-
-    // First OLED display unit (for user input)
+    // First OLED display unit
     wire one_frame_begin;
     wire one_sample_pixel;
     wire [12:0]JB_pixel_index;
     wire one_sending_pixels;
     wire [15:0]JB_oled_data;
 
+    // Second OLED display unit
+    wire two_frame_begin;
+    wire two_sample_pixel;
+    wire [12:0]JA_pixel_index;
+    wire two_sending_pixels;
+    wire [15:0]JA_oled_data;
+    
+    // System control signals
+    wire reset = sw[15];                   // Use SW15 for reset
+    wire is_arithmetic_mode = 1'b1;        // Always enabled for testing
+    
+    // Mouse placeholder signals (not used for arithmetic testing)
+    wire [6:0] mouse_x = 0;
+    wire [6:0] mouse_y = 0;
+    wire mouse_left = 0;
+    wire mouse_middle = 0;
+
+    // ARITHMETIC MODULE - THE ONLY MODULE WE'RE TESTING
+    arithmetic_module arithmetic(
+        .clk_6p25MHz(clk_6p25MHz),
+        .clk_1kHz(clk_1kHz),
+        .btnC(btnC),
+        .btnU(btnU),
+        .btnD(btnD),
+        .btnL(btnL),
+        .btnR(btnR),
+        .reset(reset),
+        .is_arithmetic_mode(is_arithmetic_mode),
+        .xpos(mouse_x),
+        .ypos(mouse_y),
+        .use_mouse(1'b0),                  // Disable mouse for testing
+        .mouse_left(mouse_left),
+        .mouse_middle(mouse_middle),
+        .one_pixel_index(JB_pixel_index),
+        .two_pixel_index(JA_pixel_index),
+        .one_oled_data(JB_oled_data),
+        .two_oled_data(JA_oled_data)
+    );
+
+    // OLED display connections
     Oled_Display first_display(
         .clk(clk_6p25MHz),
         .reset(0),
@@ -106,15 +101,7 @@ module Top_Student (
         .vccen(JB[6]),
         .pmoden(JB[7])
     );
-
-    // Second OLED display unit (to render outputs)
-    wire two_frame_begin;
-    wire two_sample_pixel;
-    wire [12:0]JA_pixel_index;
-    wire two_sending_pixels;
-    wire [15:0]JA_oled_data;
-    wire [12:0] JA_rotated_pixel_index;
-
+    
     Oled_Display second_display(
         .clk(clk_6p25MHz),
         .reset(0),
@@ -132,42 +119,8 @@ module Top_Student (
         .pmoden(JA[7])
     );
 
-    wire [15:0] table_keypad_oled_data;
-    wire [15:0] poly_input_oled_data;
-    wire [6:0] curr_x, curr_y;
-    wire is_table_mode = 1;
-    polynomial_table_module poly_table(
-        .clk_6p25MHz(clk_6p25MHz),
-        .clk_1kHz(clk_1kHz),
-        .clk_100MHz(basys_clock),
-        .btnC(btnC),
-        .btnU(btnU),
-        .btnD(btnD),
-        .btnL(btnL),
-        .btnR(btnR),
-        .xpos(curr_x),
-        .ypos(curr_y), 
-        .use_mouse(sw[0]),
-        .mouse_left(left), 
-        .mouse_middle(middle),
-        .is_table_mode(is_table_mode),
-        .coeff_a(32'h00010000), // 1.0
-        .coeff_b(32'h00020000), // 2.0
-        .coeff_c(32'h00030000), // 3.0
-        .coeff_d(32'h00040000), // 4.0
-        .one_pixel_index(JB_pixel_index),
-        .two_pixel_index(JA_rotated_pixel_index),
-        .one_oled_data(table_keypad_oled_data),
-        .two_oled_data(poly_input_oled_data)
-    );
-    wire [15:0] JB_bg_data;
-    //cursor overlay on JB screen only
-    on_screen_cursor unit_1 (.basys_clock(clk_6p25MHz),.pixel_index(JB_pixel_index),
-             .graph_mode_check(1'b1), .value(value),.setx(setx),.sety(sety),.setmax_x(setmax_x),.setmax_y(setmax_y),
-             .xpos(xpos), .ypos(ypos),.bg_data(JB_bg_data),.oled_data(JB_oled_data), .cursor_x(curr_x), .cursor_y(curr_y));
-    assign left = led[15];
-    rotate_180_for_JA rotate180(JA_pixel_index, JA_rotated_pixel_index);
-    assign JB_bg_data = is_table_mode ? table_keypad_oled_data : 0;
-    assign JA_oled_data = is_table_mode ? poly_input_oled_data : 0; 
-
+    // Basic LED indicators
+    assign led[0] = is_arithmetic_mode;    // Arithmetic mode active
+    assign led[1] = reset;                 // Reset status
+    
 endmodule

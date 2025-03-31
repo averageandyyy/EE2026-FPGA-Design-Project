410fadc63c61294a68141aec84ff85ca0d8bc17b
HoWeiHao <66128067+HoWeiHao@users.noreply.github.com>
2025-04-01 00:27:55 +0800

    Fix segment display

--- FDP.srcs/sources_1/new/render_segments.v
+++ FDP.srcs/sources_1/new/render_segments.v
@@ -21,26 +21,29 @@
 
 
 module render_segments(
-    input basys_clk,
+    input clk,
     input is_integrate,
     output reg [3:0]an,
     output reg [7:0]seg
     );
     
-    parameter [7:0]I = 8'b1_11111001;
+    parameter [7:0]I = 8'b1_11110000;
     parameter [7:0]N = 8'b0_0101011;
     parameter [7:0]T = 8'b1_0111001;
-    parameter [7:0]G = 8'b1_0100000;
+    parameter [7:0]G = 8'b1_0010000;
+    parameter [7:0]P = 8'b1_0001100;
+    parameter [7:0]L = 8'b1_1111001;
+    parameter [7:0]O = 8'b1_0100011;
     
     reg [1:0] state;
     
     wire clk_500Hz;
-    flexible_clock_divider clk_500Hz_gen(.main_clock(basys_clk), .ticks(99999), .output_clock(clk_500Hz));
+    flexible_clock_divider clk_500Hz_gen(.main_clock(clk), .ticks(6249), .output_clock(clk_500Hz));
     
     initial begin
         state = 2'b0;
         an = 4'b1111;
-        seg = 8'b1_1111111;
+        seg = 8'b11111111;
     end
     
     always @ (posedge clk_500Hz) begin
@@ -60,6 +63,25 @@ module render_segments(
             else if (state == 2'b11) begin
                 an <= 4'b1110;
                 seg <= G;
+            end
+            
+            state <= state + 1;
+        end else begin
+            if (state == 2'b00) begin
+                an <= 4'b0111;
+                seg <= P;
+            end
+            else if (state == 2'b01) begin
+                an <= 4'b1011;
+                seg <= L;
+            end
+            else if (state == 2'b10) begin
+                an <= 4'b1101;
+                seg <= O;
+            end
+            else if (state == 2'b11) begin
+                an <= 4'b1110;
+                seg <= T;
             end
             
             state <= state + 1;
